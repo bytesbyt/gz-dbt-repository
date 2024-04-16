5685243c640b3d99149b3d951162be8f0d84aa04
Tes <tesryu39@gmail.com>
2024-04-16 21:00:19 +0000

    Add command to build operational margin.

---
+++ models/intermediate/int_orders_operational.sql
@@ -0,0 +1,16 @@
+SELECT
+    marg.orders_id
+    , date_date
+    , ROUND((revenue + shipping_fee) - (logcost + ship_cost),2) AS operational_margin
+    , revenue
+    , quantity
+    , purchase_cost
+    , margin
+    , shipping_fee
+    , logcost
+    , ship_cost
+FROM
+    {{ref("int_orders_margin")}} AS marg
+JOIN
+    {{ref("stg_raw__ship")}} AS ship
+ON marg.orders_id = ship.orders_id
