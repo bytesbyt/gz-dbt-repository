3303cb7a842dd84c78be8fc270b6ee312bc2d408
Tes <tesryu39@gmail.com>
2024-04-16 21:20:54 +0000

    Update calculation for operational margin.

--- models/intermediate/int_orders_operational.sql
+++ models/intermediate/int_orders_operational.sql
@@ -1,18 +1,18 @@
 SELECT
-    marg.orders_id
-    , date_date
-    , ROUND((revenue + shipping_fee) - (logcost + ship_cost),2) AS operational_margin
-    , revenue
-    , quantity
-    , purchase_cost
-    , margin
-    , shipping_fee
-    , logcost
-    , ship_cost
+    operational.orders_id
+    , operational.date_date
+    , ROUND((operational.margin + ship.shipping_fee) - (ship.logcost + ship.ship_cost),2) AS operational_margin
+    , operational.revenue
+    , operational.quantity
+    , operational.purchase_cost
+    , operational.margin
+    , ship.shipping_fee
+    , ship.logcost
+    , ship.ship_cost
 FROM
-    {{ref("int_orders_margin")}} AS marg
+    {{ref("int_orders_margin")}} AS operational
 JOIN
     {{ref("stg_raw__ship")}} AS ship
-ON marg.orders_id = ship.orders_id
+ON operational.orders_id = ship.orders_id
 ORDER BY
     orders_id DESC
